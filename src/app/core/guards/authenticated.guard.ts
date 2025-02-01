308cb47eee33698def1d90cc80d772e67d5d222a
paoyako24 <paolaparedes248@gmail.com>
2025-02-01 13:01:49 -0600

    Add files via upload

---
+++ src/app/core/guards/authenticated.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn,Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const AuthenticatedGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if(authService.isAuthenticated()) {
+    return router.navigate(['/dashboard']);
+  }else{
+    return true;
+  }
+};
+
